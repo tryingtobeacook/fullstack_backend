ea59ed0bed6a8b36ff1762dcc72fb1a8bc8f0452
tryingtobeacook <107980914+tryingtobeacook@users.noreply.github.com>
2023-10-10 21:45:48 +0530

    First commit

---
+++ src/test/java/com/codewitharjun/fullstackbackend/controller/UserControllerTest.java
@@ -0,0 +1,28 @@
+package com.codewitharjun.fullstackbackend.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest extends UserController {
+
+    @Test
+    public void testNewUser() {
+    }
+
+    @Test
+    public void testGetAllUsers() {
+    }
+
+    @Test
+    public void testGetUserById() {
+    }
+
+    @Test
+    public void testUpdateUser() {
+    }
+
+    @Test
+    public void testDeleteUser() {
+    }
+}
